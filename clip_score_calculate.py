5535a9d39b1140c9f58f549c7df080e0270b54cb
Bhrij Patel <bhrij.patel@duke.edu>
2024-04-24 16:20:57 -0400

    Update clip_score_calculate.py

--- clip_score_calculate.py
+++ clip_score_calculate.py
@@ -3,13 +3,13 @@ import torchvision.transforms as T
 from PIL import Image
 from transformers import CLIPProcessor, CLIPTokenizer, CLIPModel
 import torch
-from diffusers import StableDiffusionPipeline, EulerDiscreteScheduler
+from diffusers import StableDiffusionPipeline #, EulerDiscreteScheduler
 
 model_id = "stabilityai/stable-diffusion-2"
 
 # Use the Euler scheduler here instead
-scheduler = EulerDiscreteScheduler.from_pretrained(model_id, subfolder="scheduler")
-pipe = StableDiffusionPipeline.from_pretrained(model_id, scheduler=scheduler, torch_dtype=torch.float16)
+# scheduler = EulerDiscreteScheduler.from_pretrained(model_id, subfolder="scheduler")
+pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
 pipe = pipe.to("cuda")
 
 prompt = "a photo of an astronaut riding a horse on mars"
