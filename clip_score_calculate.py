042fd16ebd0ce9ac7dae43c542daed14550c19ab
Bhrij Patel <bhrij.patel@duke.edu>
2024-04-24 14:10:26 -0400

    Update clip_score_calculate.py

--- clip_score_calculate.py
+++ clip_score_calculate.py
@@ -6,16 +6,16 @@ import torch
 from diffusers import StableDiffusionPipeline
 
 
-from diffusers import AutoPipelineForText2Image
-import torch
+# from diffusers import AutoPipelineForText2Image
 
-pipeline_text2image = AutoPipelineForText2Image.from_pretrained(
-    "stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=torch.float16, variant="fp16", use_safetensors=True
-).to("cuda")
 
-prompt = "Astronaut in a jungle, cold color palette, muted colors, detailed, 8k"
-image = pipeline_text2image(prompt=prompt).images[0]
-image.save('astro.png')
+# pipeline_text2image = AutoPipelineForText2Image.from_pretrained(
+#     "stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=torch.float16, variant="fp16", use_safetensors=True
+# ).to("cuda")
+
+# prompt = "Astronaut in a jungle, cold color palette, muted colors, detailed, 8k"
+# image = pipeline_text2image(prompt=prompt).images[0]
+# image.save('astro.png')
 
 pipe = StableDiffusionPipeline.from_pretrained(
 	"CompVis/stable-diffusion-v1-4", 
